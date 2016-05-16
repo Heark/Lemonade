7335f3e092c7ad28436006211a77c63dcb7010eb
Heark <antwauntune25@wcpsstudent.org>
2016-05-16 09:11:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,84 +1,67 @@
-//
-// # SimpleServer
-//
-// A simple chat server using Socket.IO, Express, and Async.
-//
-var http = require('http');
-var path = require('path');
-
-var async = require('async');
-var socketio = require('socket.io');
-var express = require('express');
-
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+var http = require("http");
+var path = require("path");
+var async = require("async");
+var socketio = require("socket.io");
+var express = require("express");
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
-
-router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(express.static(path.resolve(__dirname, "client")));
+/** @type {Array} */
 var messages = [];
+/** @type {Array} */
 var sockets = [];
-
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
+io.on("connection", function(socket) {
+  messages.forEach(function(data) {
+    socket.emit("message", data);
+  });
+  sockets.push(socket);
+  socket.on("disconnect", function() {
+    sockets.splice(sockets.indexOf(socket), 1);
+    updateRoster();
+  });
+  socket.on("message", function(prefix) {
+    /** @type {string} */
+    var part = String(prefix || "");
+    if (!part) {
+      return;
+    }
+    socket.get("name", function(dataAndEvents, errorName) {
+      var data = {
+        name : errorName,
+        text : part
+      };
+      broadcast("message", data);
+      messages.push(data);
     });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
+  });
+  socket.on("identify", function(prefix) {
+    socket.set("name", String(prefix || "Anonymous"), function(dataAndEvents) {
       updateRoster();
     });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
   });
-
+});
+/**
+ * @return {undefined}
+ */
 function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
+  async.map(sockets, function(handle, data) {
+    handle.get("name", data);
+  }, function(dataAndEvents, inplace) {
+    broadcast("roster", inplace);
   });
 }
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+/**
+ * @param {string} message
+ * @param {?} data
+ * @return {undefined}
+ */
+function broadcast(message, data) {
+  sockets.forEach(function(socket) {
+    socket.emit(message, data);
+  });
+}
+server.listen(process.env.PORT || 3E3, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
