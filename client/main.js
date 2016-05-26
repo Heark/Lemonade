3b46a48db382d0b2ef734587382485eef028e76f
Heark <antwauntune25@wcpsstudent.org>
2016-05-26 14:19:44 -0400

    add object.watch

--- client/main.js
+++ client/main.js
@@ -1,30 +1,76 @@
 
 var lemon_money = 0;
 var lemon_growth = 1;
-var upgrade = function(tmplName, desc, price, effect) {
+var Lemonade = {
+upgrade: function(tmplName, desc, price, effect) {
   this.name = tmplName;
   this.desc = desc;
   this.price = price;
   this.effect = effect;
   this.clicked = false;
-};
-function float(num) {
+},
+float: function(num) {
   this.num = num;
   num = num.toString();
   return parseFloat(num);
 }
-var Grandma = new upgrade("Grandma's Recipe", "", 100, 5);
-var Orange = new upgrade("Orange Juice", "", 350, 8);
-var Assistant = new Upgrade("Assistant", "", 750, float(10.5));
-var upgrades = [Grandma, Orange, Assistant];
-$(document).ready(function() {
-  var i = 0;
-  for (;i < upgrades.length;i++) {
-    $("#upgrades").append('<a href="#" id="up' + i + '" class="btn btn-sml btn-success" title=' + upgrades[i].desc + ">" + upgrades[i].name + "</a>");
-  }
+}
+  if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
 
-  $("#lemon").click(function() {
-    lemon_money += lemon_growth;
-    $("#lemonmoney").html("$" + lemon_money.toString());
-  });
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+var $watchLemon = {
+  p: lemon_money
+};
+setInterval(function(){
+  $watchLemon.p = lemon_money;
+}, 500)
+$watchLemon.watch("p", function (id, oldval, newval) {
+    newval > oldval ? $("#lemonmoney").html("$" + lemon_money.toString()) : $("#lemonmoney").html("$" + lemon_money.toString());
+    return newval;
 });
+$(document).ready(function(){
+    $("#lemon").click(function() {
+    lemon_money += lemon_growth;
+  });
+})
+
